c724a552993f2ac1291bfc6c8f028d0447c01185
David Flemström <dflemstr@users.noreply.github.com>
2018-06-24 22:59:10 +0200

    Require Send and Sync for serializers

--- src/serializer/mod.rs
+++ src/serializer/mod.rs
@@ -3,7 +3,7 @@ use ::measurement::Measurement;
 pub mod line;
 
 /// `Measurement` serializer.
-pub trait Serializer {
+pub trait Serializer: Send + Sync {
     /// Serializes measurement to String.
     fn serialize(&self, measurement: &Measurement) -> String;
 }
